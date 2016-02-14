6020577a5f8e836819cd343af3dfebfc9d44a584
posttragedy <traga@inbox.ru>
2016-02-14 21:45:43 +0300

    Update script.js

--- dev_assets/js/script.js
+++ dev_assets/js/script.js
@@ -1,8 +1,1 @@
-$(document).ready(function(){
-	var timer =  document.getElementById('timer');
-	var counter = 0;
-	setInterval(function() {
-		timer.innerText = counter;
-		counter++;
-	}, 1000)
-});
+
